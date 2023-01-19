362775b8f6b50e9c3e54a0a51eac24d31d2af389
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-19 15:43:03 -0400

    Add components and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Books from './pages/Books'
+import Categories from './pages/Categories'
+import Navbar from './component/Navbar'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/pages/Books.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddBook from '../component/AddBook'
+import Book from '../component/Book'
+
+export default function Books() {
+  return (
+    <main>
+      <Book />
+      <AddBook />
+    </main>
+  )
+}
